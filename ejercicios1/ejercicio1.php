b223ec7204de292eca0eab451de690a3c79c08f4
enderson1995 <enderson19952009@gmail.com>
2020-07-22 23:54:22 -0400

    primeros ejercicios resueltos

---
+++ ejercicios1/ejercicio1.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+   El enunciado
+   Crear 2 variables llamadas
+   país, cotinente
+   iniciar con valor imprimirlo comentario explicativo de las varibles
+*/
+
+$pais = "Venezuela"; // string
+$continente = "America";
+
+echo"<h1>".$pais." se encuentra en ".$continente."</h1>";
+
+echo "<p>".var_dump($pais)."</p>";
