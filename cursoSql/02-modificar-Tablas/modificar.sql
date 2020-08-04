1e9695d110baa563b1ca5c0d43dab21cbe8d2d4a
enderson1995 <enderson19952009@gmail.com>
2020-08-04 14:44:50 -0400

    crear y restringir base de datos

--- cursoSql/02-modificar-Tablas/modificar.sql
+++ cursoSql/02-modificar-Tablas/modificar.sql
@@ -12,3 +12,9 @@ ALTER TABLE usuarios_renom MODIFY apellido char(40) not null;
 
 # añadir coumna  #
 ALTER TABLE usuarios_renom ADD website varchar(100) null;
+
+# añadir resticcion a columna #
+ALTER TABLE usuarios_renom ADD CONSTRAINT uq_email UNIQUE(email);
+
+# BORRAR UNA COLUMNA #
+ALTER TABLE usuarios_renom DROP website;
