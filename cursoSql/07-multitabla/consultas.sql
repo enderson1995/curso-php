3804b21d4a352b739fe66be87888f5c28a8d128e
enderson1995 <enderson19952009@gmail.com>
2020-08-11 14:43:39 -0400

    clases de sql

---
+++ cursoSql/07-multitabla/consultas.sql
@@ -0,0 +1,6 @@
+/* 
+    Consultar varias tablas en una sola sentencia
+    Funciona para mostrar la informacion relacionadas entre distintas talas y así ofrecer una información 
+    completa
+*/
+
