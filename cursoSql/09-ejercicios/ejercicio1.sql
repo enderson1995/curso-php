847b74a6d0b3c3fdcc35550985d3bd620d9a9ae2
enderson1995 <enderson19952009@gmail.com>
2020-08-17 14:55:07 -0400

    llenar formulario

--- cursoSql/09-ejercicios/ejercicio1.sql
+++ cursoSql/09-ejercicios/ejercicio1.sql
@@ -6,24 +6,27 @@
 
 /* tabla de carros */
 
-CREATE TABLE carros{
-id          int(10) auto_icrement not null,
+CREATE DATABASE IF NOT EXISTS concensionario;
+USE concensionario;
+
+CREATE TABLE carros(
+id          int(10) auto_increment not null,
 modelo      varchar(100) not null,
 marca       varchar(50),
 precio      int(20) not null,
 stock       int(255) not null,
 CONSTRAINT pk_carros PRIMARY KEY(id)
-}ENGINE=InnoDB;
+)ENGINE=InnoDB;
 
-CREATE TABLE grupos{
-id          int(10) auto_icrement not null,
+CREATE TABLE grupos(
+id          int(10) auto_increment not null,
 nombre      varchar(100) not null,
 ciudad      varchar(100),
 CONSTRAINT pk_grupos PRIMARY KEY(id)
-}ENGINE=InnoDB;
+)ENGINE=InnoDB;
 
-CREATE TABLE vendedores{
-id          int(10) auto_icrement not null,
+CREATE TABLE vendedores(
+id          int(10) auto_increment not null,
 grupo_id    int(10) not null,
 jefe        int(10),
 nombre      varchar(100) not null,
@@ -35,10 +38,10 @@ comision    float(10,2),
 CONSTRAINT pk_vendedores PRIMARY KEY(id),
 CONSTRAINT fk_vendedor_grupo FOREIGN KEY(grupo_id) REFERENCES grupos(id),
 CONSTRAINT fk_vendedor_feje FOREIGN KEY(jefe) REFERENCES vendedores(id)
-}ENGINE=InnoDB;
+)ENGINE=InnoDB;
 
-CREATE TABLE clientes{
-id              int(10) auto_icrement not null,
+CREATE TABLE clientes(
+id              int(10) auto_increment not null,
 vendedor_id     int(10),
 nombre          varchar(150) not null,
 ciudad          varchar(100),
@@ -46,15 +49,15 @@ gastado         float(50,2),
 fecha           date,
 CONSTRAINT pk_clientes PRIMARY KEY(id),
 CONSTRAINT fk_cliente_vendedor FOREIGN KEY(vendedor_id) REFERENCES vendedores(id)
-}ENGINE=InnoDB;
+)ENGINE=InnoDB;
 
-CREATE TABLE pedidos{
-id          int(10) auto_icrement not null,        
+CREATE TABLE pedidos(
+id          int(10) auto_increment not null,        
 cliente_id  int(10) not null,
 carro_id    int(10) not null,
-cantidad    int(5),
+cantidad    int(100),
 fecha       date,
 CONSTRAINT pk_pedidos PRIMARY KEY(id),
-CONSTRAINT fk_cliente FOREIGN key(cliente_id) REFERENCES clientes(id),
-CONSTRAINT fk_carro FOREIGN key(carro_id) REFERENCES carros(id)
-}ENGINE=InnoDB;
+CONSTRAINT fk_pedido_cliente FOREIGN key(cliente_id) REFERENCES clientes(id),
+CONSTRAINT fk_pedido_carro FOREIGN key(carro_id) REFERENCES carros(id)
+)ENGINE=InnoDB;
